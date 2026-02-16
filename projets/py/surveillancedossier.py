055a940d036375776e480ef01e8fdc5f322371db
RayGarrathy <165572413+RayGarrathy@users.noreply.github.com>
2026-02-16 15:39:06 +0100

    Add file system watcher for new report notifications
    Implement a file system watcher that sends notifications for new reports.

--- projets/py/surveillancedossier.py
+++ projets/py/surveillancedossier.py
@@ -1,1 +1,47 @@
+import time
+import os
+from pathlib import Path
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+from winotify import Notification
 
+# Dossier réseau à surveiller
+DOSSIER_A_SURVEILLER = r"\\Alba\spr\PARTAGES\LARP\LARC\LARC_SUPPORT\RAPPORTS A VALIDER"
+
+class Handler(FileSystemEventHandler):
+    
+    def on_created(self, event):
+        if not event.is_directory:
+            file = Path(event.src_path).name
+            dir_name = os.path.basename(os.path.dirname(event.src_path))
+
+            notif = Notification(
+                app_id=f"Notification {dir_name}",
+                title="Un nouveau rapport est disponible",
+                msg=file,
+                duration="short",
+                icon=r"C:\ProgramData\SurveillanceDossier\papier.ico"
+            )
+
+            notif.add_actions(
+                label=f"Ouvrir {dir_name}",
+                launch=os.path.dirname(event.src_path)
+            )
+
+            notif.show()
+
+def main():
+    observer = Observer()
+    observer.schedule(Handler(), DOSSIER_A_SURVEILLER, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+if __name__ == "__main__":
+    main()
